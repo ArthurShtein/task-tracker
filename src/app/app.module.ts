1dee30d45c2b5fd99cdeed2b7e04a949cae73459
Spinier <artsh94@gmail.com>
2021-08-18 13:42:38 +0300

    Task tracker app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './cmps/header/header.component';
+import { ButtonComponent } from './cmps/button/button.component';
+import { TasksComponent } from './cmps/tasks/tasks.component';
+import { TaskItemComponent } from './cmps/task-item/task-item.component';
+import { AddTaskComponent } from './cmps/add-task/add-task.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FooterComponent } from './cmps/footer/footer.component';
 
+const appRoutes: Routes = [
+  { path: '', component: TasksComponent },
+  { path: 'about', component: AboutComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+''
